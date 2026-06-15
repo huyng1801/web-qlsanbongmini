b04ba5907e059c95ad758061e29cd793d3a320ab
huyng1801 <huynguyendev18012003@gmail.com>
2026-06-15 20:11:51 +0700

    Stop service before publish on VPS quick update

--- deploy/quick_update.sh
+++ deploy/quick_update.sh
@@ -8,11 +8,14 @@ export PATH="/usr/share/dotnet:${PATH}"
 cd "$APP_DIR"
 git pull origin master
 
+systemctl stop qlsanbongmini || true
+sleep 2
+
 cd QLSanBongMini.Web
 dotnet publish -c Release -r linux-x64 --self-contained true -o "$APP_DIR/publish" /p:UseAppHost=true
 cp -r Views "$APP_DIR/publish/Views"
 chown -R www-data:www-data "$APP_DIR/publish"
-systemctl restart qlsanbongmini
+systemctl start qlsanbongmini
 sleep 8
 curl -s -o /dev/null -w "HTTP %{http_code}\n" http://127.0.0.1/Account/Login
 echo "Quick update done"
